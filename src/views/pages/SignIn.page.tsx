814cfd1d34ef3102bc1d5bf871b57fbdf9c93e63
ryuji <ryu.seven.h7420@gmail.com>
2020-11-05 09:52:25 +0900

    SignInページの修正

--- src/views/pages/SignIn.page.tsx
+++ src/views/pages/SignIn.page.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 // import { RouteComponentProps } from 'react-router-dom';
 // import * as H from 'history';
@@ -45,15 +45,66 @@ const useStyles = makeStyles((theme: Theme) =>
     submit: {
       margin: theme.spacing(3, 0, 2),
     },
+    question: {
+      display: 'inline-block',
+      width: '100%',
+      textAlign: 'center',
+    },
+    errorMessage: {
+      color: 'red',
+    },
   })
 );
 
 const SignInPage: React.FC = () => {
   const classes = useStyles();
   const { register, errors, handleSubmit } = useForm();
+  const [isEmail, setIsEmail] = useState<boolean>(false);
+  const [emailError, setEmailError] = useState<boolean>(false);
+  const [emailErrorMessage, setEmailErrorMessage] = useState<string>('');
+  const [isPassword, setIsPassword] = useState<boolean>(false);
+  const [passwordError, setPasswordError] = useState<boolean>(false);
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState<string>('');
+
+  useEffect(() => {
+    console.log('effected');
+  }, [
+    isEmail,
+    emailError,
+    emailErrorMessage,
+    isPassword,
+    passwordError,
+    passwordErrorMessage,
+  ]);
 
   const handleSignIn = () => {
     console.log('called');
+  };
+
+  const checkEmailInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    if (!value) {
+      setEmailError(true);
+      setEmailErrorMessage('Please enter your email address');
+      setIsEmail(false);
+    } else {
+      setEmailError(false);
+      setEmailErrorMessage('');
+      setIsEmail(true);
+    }
+  };
+
+  const checkPasswordInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    if (!value) {
+      setPasswordError(true);
+      setPasswordErrorMessage('Please enter your password');
+      setIsPassword(false);
+    } else {
+      setPasswordError(false);
+      setPasswordErrorMessage('');
+      setIsPassword(true);
+    }
   };
 
   return (
@@ -71,7 +122,11 @@ const SignInPage: React.FC = () => {
           noValidate
           onSubmit={handleSubmit(handleSignIn)}
         >
+          {errors.email && errors.email.type === 'pattern' ? (
+            <div className={classes.errorMessage}>Not match email pattern</div>
+          ) : null}
           <TextField
+            error={emailError}
             variant="outlined"
             margin="normal"
             required
@@ -81,18 +136,17 @@ const SignInPage: React.FC = () => {
             name="email"
             autoComplete="email"
             autoFocus
+            helperText={emailErrorMessage}
             inputRef={register({
-              required: {
-                value: true,
-                message: 'Please enter your email address',
-              },
-              pattern: {
-                value: /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/,
-                message: 'Not correct email format',
-              },
+              required: true,
+              pattern: /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/,
             })}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              checkEmailInput(e)
+            }
           />
           <TextField
+            error={passwordError}
             variant="outlined"
             margin="normal"
             required
@@ -102,33 +156,31 @@ const SignInPage: React.FC = () => {
             type="password"
             id="password"
             autoComplete="current-password"
+            helperText={passwordErrorMessage}
+            inputRef={register({
+              required: true,
+            })}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              checkPasswordInput(e)
+            }
           />
           <FormControlLabel
             control={<Checkbox value="remenber" color="primary" />}
             label="Remember me"
           />
           <Button
+            disabled={isEmail && isPassword ? false : true}
             type="submit"
             fullWidth
             variant="contained"
             color="primary"
             className={classes.submit}
-            // href="/top"
           >
             Sign In
           </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forget password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="/signup" variant="body2">
-                Don't have an account? Sign Up
-              </Link>
-            </Grid>
-          </Grid>
+          <Link href="/signup" variant="body2" className={classes.question}>
+            Don't have an account? Sign Up
+          </Link>
         </form>
       </div>
       <Box mt={8}>
