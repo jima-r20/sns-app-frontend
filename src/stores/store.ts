1c9e3cd4e51f31d6a729401f85832fa5fd02c8a4
ryuji <ryu.seven.h7420@gmail.com>
2020-11-23 22:17:44 +0900

    デフォルトで用意されていたCounter関係削除

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -2,14 +2,12 @@ import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import userReducer from './slices/user.slice';
 import postReducer from './slices/post.slice';
 import pageReducer from './slices/page.slice';
-import counterReducer from './slices/counterSlice';
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     post: postReducer,
     page: pageReducer,
-    counter: counterReducer,  // あとで消す
   },
 });
 
