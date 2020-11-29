5f87fef4391bd43f065247ec5661a7b3b77212f8
ryuji <ryu.seven.h7420@gmail.com>
2020-11-30 00:20:58 +0900

    左サイドバーのapproveボタンを押した場合の承認機能実装

--- src/views/components/FriendRequestItem.tsx
+++ src/views/components/FriendRequestItem.tsx
@@ -1,18 +1,49 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Grid, Avatar, Typography, Card, Chip } from '@material-ui/core';
 import { selectUsers } from '../../stores/slices/user.slice';
 import { FriendRequestItmStyle } from '../../styles/FriendRequestItem.style';
+import { AppDispatch } from '../../stores/store';
+import {
+  resetApproveOrUnfollowButtomClicked,
+  setApproveOrUnfollowButtomClicked,
+} from '../../stores/slices/page.slice';
+import {
+  fetchApproveRequest,
+  fetchCreateFollow,
+  selectFollows,
+} from '../../stores/slices/follow.slice';
 
 interface PROPS_FRIEND_REQUEST {
   askFrom: number;
 }
 
 const FriendRequestItem: React.FC<PROPS_FRIEND_REQUEST> = (props) => {
+  const dispatch: AppDispatch = useDispatch();
   const classes = FriendRequestItmStyle();
   const { askFrom } = props;
+  const follows = useSelector(selectFollows);
   const user = useSelector(selectUsers).find((u) => u.id === askFrom);
   const avatarIcon = user?.displayName.charAt(0).toUpperCase();
+
+  const onClickApproveRequest = async () => {
+    dispatch(setApproveOrUnfollowButtomClicked());
+    // リクエストbodyにはboolean型ではなくstring型でtrueと渡す必要あり
+    await dispatch(fetchApproveRequest({ askFrom, approved: 'true' }));
+    const found = follows.find((f) => f.askTo === askFrom);
+    console.log(`found: ${found}`);
+    if (found) {
+      // リクエストを承認したユーザをフォローしていた場合、
+      // 自身のフォローリクエストのapprovedをtrueにする
+      await dispatch(fetchApproveRequest({ askTo: askFrom, approved: 'true' }));
+    }
+    if (!found) {
+      // リクエストを承認したユーザをフォローしていなかった場合には、
+      // approvedをtrueとしてフォローする
+      await dispatch(fetchCreateFollow({ askTo: askFrom, approved: 'true' }));
+    }
+    dispatch(resetApproveOrUnfollowButtomClicked());
+  };
 
   return (
     <React.Fragment>
@@ -27,8 +58,13 @@ const FriendRequestItem: React.FC<PROPS_FRIEND_REQUEST> = (props) => {
             </Typography>
           </Grid>
           <Grid item xs={7} className={classes.buttons}>
-            <Chip clickable color="primary" size="small" label="Approve" />
-            <Chip clickable color="secondary" size="small" label="Reject" />
+            <Chip
+              clickable
+              color="primary"
+              size="small"
+              label="Approve"
+              onClick={onClickApproveRequest}
+            />
           </Grid>
         </Grid>
       </Card>
